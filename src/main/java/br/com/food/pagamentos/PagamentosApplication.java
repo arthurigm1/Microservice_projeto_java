032183a08fcaf144f59e111a0decec0d7f14939b
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-01-14 11:12:14 -0300

    apipagamento_done

--- src/main/java/br/com/food/pagamentos/PagamentosApplication.java
+++ src/main/java/br/com/food/pagamentos/PagamentosApplication.java
@@ -2,7 +2,9 @@ package br.com.food.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class PagamentosApplication {
 
